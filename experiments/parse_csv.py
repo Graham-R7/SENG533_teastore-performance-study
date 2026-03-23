07e4a167e7ac0bf10277f9293fb58d0171ccbb01
Graham-R7 <Ryan.graham3@ucalgary.ca>
2026-03-22 22:27:16 -0600

    Working baseline It's messy, dead files everywhere. Needs Ubuntu. Used Windows with WSL, make sure to connect with docker. If you come across points where you don't know the code to use to run something, let me know

--- experiments/parse_csv.py
+++ experiments/parse_csv.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv("../results/raw/baseline.csv")
+df = pd.read_csv("../results/baseline.csv")
 print(df.head())
--- experiments/run-baseline.sh
+++ experiments/run-baseline.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-PROFILE=workload/baseline.arrival
-SCRIPT=workload/browsing.lua
-OUT=results/raw/baseline.csv
+PROFILE=../workload/baseline.arrival
+SCRIPT=../workload/teastore_browse.lua
+OUT=results/baseline.csv
 
 echo "Running baseline..."
 
-load-generator \
+java -jar httploadgenerator.jar \
   --arrival-rate $PROFILE \
   --script $SCRIPT \
+  --base-url http://localhost:8080 \
   --output $OUT
